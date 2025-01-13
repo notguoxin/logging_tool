e63c76b416e295755f76b9f54e218241ac55d80d
GuoXin <githublol@guoxin.win>
2025-01-14 07:08:56 +0800

    Added bunch of stuff

--- simplelog.py
+++ simplelog.py
@@ -10,16 +10,18 @@ class Logger:
         log_to_file: bool = False,
         log_DEBUG_tofile: bool = False,
         logpath: str = None,
+        loglevels: list = None,
     ):
         colorama.just_fix_windows_console()  # Initialize colorama once
         self.log_to_file = log_to_file
         self.logpath = logpath
         self.log_DEBUG_tofile = log_DEBUG_tofile
+        self.loglevels = loglevels
 
     def _has_multiple_lines(self, s: str) -> bool:
         try:
             return s.count("\n") > 0
-        except AttributeError:
+        except Exception:
             return False
 
     def _write_to_file(self, log_str: str, log_type: str) -> None:
@@ -34,34 +36,47 @@ class Logger:
             with open(self.logpath, "w") as log_file:
                 log_file.write(log_str + "\n")  # Add newline for each log entry
 
-    def _log(self, input: str, prefix: str, color: str) -> None:
-        """Handles the logging functionality."""
-        if self._has_multiple_lines(input):
-            self._log_multiline(input, prefix, color)
-
+    def _getlog_string(self, input: str, prefix: str, level: str) -> str:
+        if self.loglevels and level:
+            if level in self.loglevels:
+                log_str = (
+                    f"{datetime.datetime.now().isoformat()} {prefix} [{level}]: {input}"
+                )
+            else:
+                raise ValueError
         else:
             log_str = f"{datetime.datetime.now().isoformat()} {prefix}: {input}"
+        return log_str
+
+    def _log(self, input: str, prefix: str, level: str, color: str) -> None:
+        """Handles the logging functionality."""
+        if self._has_multiple_lines(input):
+            self._log_multiline(input, prefix, level, color)
+
+        else:
+            log_str = self._getlog_string(input, prefix, level)
+
             print(termcolor.colored(log_str, color))
             if self.log_to_file and self.logpath:
                 self._write_to_file(log_str, prefix)
 
-    def _log_multiline(self, input: str, prefix: str, color: str) -> None:
+    def _log_multiline(self, input: str, prefix: str, level: str, color: str) -> None:
         def handler(splited_input):
-            return f"{datetime.datetime.now().isoformat()} {prefix}: {splited_input}"
+            return self._getlog_string(input, prefix, level)
 
         for line in input.splitlines():
             print(termcolor.colored(handler(line), color))
             if self.log_to_file and self.logpath:
                 self._write_to_file(handler(line), prefix)
 
-    def debug(self, input: str) -> None:
-        self._log(input, "DEBUG", "light_blue")
+    def debug(self, input: str, level: str = "") -> None:
+        self._log(input, "DEBUG", level, "light_blue")
 
-    def log(self, input: str) -> None:
-        self._log(input, " INFO", "green")
+    def log(self, input: str, level: str = "") -> None:
+        self._log(input, " INFO", level, "green")
 
-    def warn(self, input: str) -> None:
-        self._log(input, " WARN", "yellow")
+    def warn(self, input: str, level: str = "") -> None:
+        self._log(input, " WARN", level, "yellow")
 
-    def error(self, input: str) -> None:
-        self._log(input, "ERROR", "red")
+    def error(self, input: str, level: str = "") -> None:
+        self._log(input, "ERROR", level, "red")
